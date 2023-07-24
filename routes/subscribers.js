21548a1c9e68e6d90a691bf1ad87600552382f15
Ishaan Rawat <i.rawat98@gmail.com>
2023-07-24 21:43:03 +0100

    created schema to refer to subscriber and info related

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,22 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
+// get all subs
 router.get('/', (req, res) => {
-    res.send('Hellow World')
+    res.send('Hello World')
 })
 
-router.get('/id', (req, res) => {
-
+// get one sub
+router.get('/:id', (req, res) => {
+    res.send(req.params.id);
 })
 
+// create sub
 router.post('/', (req, res) => {
 
 })
 
+// update sub
 router.patch('/:id', (req, res) => {
 
 })
 
+// delete sub
 router.delete('/:id', (req, res) => {
 
 })
