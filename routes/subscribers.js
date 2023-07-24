851fa1eba5015bb5fd50644a4e327eee24577749
Ishaan Rawat <i.rawat98@gmail.com>
2023-07-24 21:57:48 +0100

    attempting to create subscriber

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const Subscriber = require('../models/subscriber')
 
 // get all subs
-router.get('/', (req, res) => {
-    res.send('Hello World')
+router.get('/',  async (req, res) => {
+    try {
+    const subscribers = await Subscriber.find()
+    res.json(subscribers)
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
 })
 
 // get one sub
