7440d8d33788d488d4d8b5ea10159b6776cd6505
Ishaan Rawat <i.rawat98@gmail.com>
2023-07-25 19:34:53 +0100

    included a few requests

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -50,7 +50,7 @@ router.patch('/:id', getSubscriber, async (req, res) => {
 // Deleting One
 router.delete('/:id', getSubscriber, async (req, res) => {
     try {
-        await res.subscriber.remove();
+        await res.subscriber.deleteOne();
         res.json({message: 'Deleted Subscriber'})
     } catch (err) {
         res.status(500).json({message: err.message})
